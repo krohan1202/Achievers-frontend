ad4b84f43120eeda527bf42a4c11197add762258
Rohan Karmakar <krohan1202@gmail.com>
2021-07-13 13:32:47 +0530

    Initial commit

---
+++ src/components/mainpages/career/JobCards.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function JobCards(props) {
+
+    const rolesAndResponsibilities = props.rolesAndResponsibilities.map((roles) => 
+        <li className="careers__jobCards-bulletPoints">{roles}</li>  
+        );
+
+    return (
+      <div className="careers__jobCards">
+          <div className="careers__jobCards--leftSide">
+            <p className="careers__jobCards--jobTitle">{props.jobTitle}</p>
+            <p className="careers__jobCards--jobHighlightsHeading">Job Highlights</p>
+            <p className="careers__jobCards--jobRequirements">{props.jobRequirements}</p>
+            <a href={props.link} className="careers__jobCards--applyBtn">Apply Now</a>
+          </div>
+
+          <div className="careers__jobCards--RightSide">
+            <p className="careers__jobCards--jobDescription">Job Description</p>
+            <p className="careers__jobCards--rolesAndResponsibilitiesHeading">Roles And Responsibilities</p>
+            {rolesAndResponsibilities}
+          </div>  
+         
+      </div>
+    );
+  }
+  
+  export default JobCards;
